8ccdbc56e01d9967fefc725e2ae6347e6aba4186
liupenga <liupenga@ctrip.com>
2018-06-13 16:58:24 +0800

    shutdown hook

---
+++ src/main/java/com/liupeng/learning/TestJVMShutdownHook.java
@@ -0,0 +1,17 @@
+package com.liupeng.learning;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class TestJVMShutdownHook {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("start ...");
+        System.out.println("sleep 5 sec");
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override public void run() {
+                System.out.println("jvm is shutdown...");
+            }
+        });
+        SECONDS.sleep(5);
+        System.out.println("end.");
+    }
+}
