bee0d7ebc02d0d0a37d8c34b704a62221ca39fb5
liupenga <liupenga@ctrip.com>
2018-07-13 16:48:09 +0800

    add substring

---
+++ src/main/java/com/liupeng/learning/basic/TesstSubstring.java
@@ -0,0 +1,8 @@
+package com.liupeng.learning.basic;
+
+public class TesstSubstring {
+    public static void main(String[] args) {
+        String a = "";
+        System.out.println(a.substring(0,a.length()));
+    }
+}
