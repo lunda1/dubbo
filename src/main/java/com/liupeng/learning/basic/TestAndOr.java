ad56520bf6747909ed36c11a991e41461384a9ca
liupenga <liupenga@ctrip.com>
2018-07-16 15:11:40 +0800

    && ||

---
+++ src/main/java/com/liupeng/learning/basic/TestAndOr.java
@@ -0,0 +1,10 @@
+package com.liupeng.learning.basic;
+
+public class TestAndOr {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = false;
+        boolean c = true;
+        System.out.println(a && b || c);
+    }
+}
