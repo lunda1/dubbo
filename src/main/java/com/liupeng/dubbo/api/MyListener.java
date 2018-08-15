79292312160e4344351886d8979510e0ed83c8d7
liupenga <liupenga@ctrip.com>
2018-08-15 19:32:54 +0800

    update

--- src/main/java/com/liupeng/dubbo/api/MyListener.java
+++ src/main/java/com/liupeng/dubbo/api/MyListener.java
@@ -1,11 +1,12 @@
 package com.liupeng.dubbo.api;
 
+import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.common.extension.Adaptive;
 import com.alibaba.dubbo.rpc.Exporter;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.alibaba.dubbo.rpc.listener.ExporterListenerAdapter;
 
-@Adaptive
+@Activate
 public class MyListener extends ExporterListenerAdapter {
     public void exported(Exporter<?> exporter) throws RpcException {
         Class<?> clz = exporter.getInvoker().getInterface();
