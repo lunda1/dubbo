a7803447b55fa7e8b672fae00344879b73f5d828
liupenga <liupenga@ctrip.com>
2018-09-18 21:24:31 +0800

    update

--- src/main/java/com/liupeng/dubbo/api/TestApiGenericConsumer.java
+++ src/main/java/com/liupeng/dubbo/api/TestApiGenericConsumer.java
@@ -1,34 +1,37 @@
 package com.liupeng.dubbo.api;
 
 import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.ReferenceConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.rpc.service.GenericService;
 
-/**
- * 适用于消费端没有API接口类和入参以及出参pojo类的情况，泛化调用通常用于框架集成，比如：实现一个通用的服务测试框架
- **/
 public class TestApiGenericConsumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ApplicationConfig applicationConfig = new ApplicationConfig();
-        applicationConfig.setName("generic-consumer");
+        applicationConfig.setName("app-api-generic-consumer");
 
         RegistryConfig registryConfig = new RegistryConfig();
         registryConfig.setAddress("127.0.0.1:2181");
         registryConfig.setProtocol("zookeeper");
 
-        ReferenceConfig<GenericService> referenceConfig = new ReferenceConfig<>();
+        ProtocolConfig protocolConfig = new ProtocolConfig();
+
+        ReferenceConfig<GenericService> referenceConfig = new ReferenceConfig<GenericService>();
         referenceConfig.setApplication(applicationConfig);
         referenceConfig.setRegistry(registryConfig);
-        referenceConfig.setVersion("1.0.0");
-        referenceConfig.setGroup("dubbo1");
-
         referenceConfig.setInterface("com.liupeng.dubbo.DemoService");
+        referenceConfig.setVersion("1.0.0");
         referenceConfig.setGeneric(true);
 
+        referenceConfig.setGroup("dubbo1");
+        referenceConfig.setTimeout(9000);
+
         GenericService genericService = referenceConfig.get();
-        Object result = genericService.$invoke("sayHello",new String[]{"java.lang.String"},new Object[]{"qqq"});
-        System.out.println(result);
+
+        System.out.println(genericService.$invoke("sayHello",new String[]{"java.lang.String"},new Object[]{"world"}));
+
+        Thread.currentThread().join();
 
     }
 }
