229f3516fa92eba516e0a78b19cee9cd1ad9cd26
liupenga <liupenga@ctrip.com>
2018-11-21 17:35:34 +0800

    update

---
+++ src/main/java/com/liupeng/learning/basic/TestJavaD.java
@@ -0,0 +1,8 @@
+package com.liupeng.learning.basic;
+
+public class TestJavaD {
+    public static void main(String[] args) {
+        //运行时添加 -Dgang.badai 代表添加系统参数gang.badai
+        System.out.println(System.getProperty("gang.badai"));
+    }
+}
