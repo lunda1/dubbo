c19c68134b7837d6c7432f0b83b01e4e85bc78c1
oliver.liu <oliver.liu@ly.com>
2019-09-24 18:59:33 +0800

    tmp

---
+++ src/main/java/com/liupeng/learning/basic/TestList.java
@@ -0,0 +1,13 @@
+package com.liupeng.learning.basic;
+
+import java.util.List;
+
+public class TestList {
+    public static void main(String[] args) {
+        List<Integer> list = null;
+        Integer a = null;
+        if (1 == a) {
+            System.out.println(a);
+        }
+    }
+}
